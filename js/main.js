f770adbd23cbfa31b524d49c94107bc87f9b784c
Hank Kuo <hank_kuo@hiiir.com>
2014-10-06 13:51:09 +0800

    1. 修改p1的下一頁按鈕, 上面加上說明文字: 我要報名 2. p2, p3加上麵包屑 3. p1~p3都加上fb分享圖示 4. 成功submit後, lightbox加上分享fb按鈕

--- js/main.js
+++ js/main.js
@@ -19,119 +19,142 @@
     var $memberInput = $('#member');
 
     var memberDataHash = {
-    	'tsai': {
-    		iconHeight: 119,
-    		mapAlt: '台北市第4選區',
-    		areaInput: '台北市/'
-    	},
-    	'lin': {
-    		iconHeight: 110,
-    		mapAlt: '新北市第6選區',
-    		areaInput: '新北市/板橋區/'
-    	},
-    	'wu': {
-    		iconHeight: 108,
-    		mapAlt: '新北市第1選區',
-    		areaInput: '新北市/'
-    	}
+        'tsai': {
+            iconHeight: 119,
+            mapAlt: '台北市第4選區',
+            areaInput: '台北市/'
+        },
+        'lin': {
+            iconHeight: 110,
+            mapAlt: '新北市第6選區',
+            areaInput: '新北市/板橋區/'
+        },
+        'wu': {
+            iconHeight: 108,
+            mapAlt: '新北市第1選區',
+            areaInput: '新北市/'
+        }
 
     };
     var memberNames = 'tasi lin wu';
     var mapHighlightOption = {
-		fill: true,
-		fillColor: 'ff0000',
-		fillOpacity: 1,
-		stroke: true,
-		strokeColor: 'ffffff',
-		strokeOpacity: 1,
-		strokeWidth: 1,
-		fade: true,
-		shadow: false,
-		shadowPosition: 'outside',
-		shadowFrom: 'fill',
-		shadowX: 10,
-		shadowY: 10,
-		shadowRadius: 1,
-		shadowColor: '0000ff',
-		shadowOpacity: 0.5
-	};
-	$map.find('img').maphilight(mapHighlightOption);
+        fill: true,
+        fillColor: 'ff0000',
+        fillOpacity: 1,
+        stroke: true,
+        strokeColor: 'ffffff',
+        strokeOpacity: 1,
+        strokeWidth: 1,
+        fade: true,
+        shadow: false,
+        shadowPosition: 'outside',
+        shadowFrom: 'fill',
+        shadowX: 10,
+        shadowY: 10,
+        shadowRadius: 1,
+        shadowColor: '0000ff',
+        shadowOpacity: 0.5
+    };
+    $map.find('img').maphilight(mapHighlightOption);
+
+    // 2014.10.05新增: 開始
+    var fbshare = function() {
+        FB.ui({
+          method: 'share_open_graph',
+          action_type: 'og.likes',
+          action_properties: JSON.stringify({
+              object: location.href,
+          })
+        }, function(response){
+
+        });
+    };
+    var p3steps = $('.p3 .steps');
+    var stepsLastLine = p3steps.find('.line').last();
+    var stepsLastCircle = p3steps.find('.circle').last();
+    // 2014.10.05新增: 結束
+
 
     $(function() {
 
-    	$('#next').on('click', function(e) {
-    		e.preventDefault();
+        $('#next').on('click', function(e) {
+            e.preventDefault();
 
-    		$p2.fadeIn(300, function() {
-    			
-	    		$body.animate({
-	                queue: false,
-	                scrollTop: $p2.offset().top,
-	            }, {
-	                duration: 1000,
-	                step: function(now, fx) {
+            $p2.fadeIn(300, function() {
 
-	                },
-	                start: function() {
-	                    $doc.disablescroll();
-	                },
-	                done: function() {
-	                    $doc.disablescroll('undo');
-	                }
-	            });
-    		});
-    	});
+                $body.animate({
+                    queue: false,
+                    scrollTop: $p2.offset().top,
+                }, {
+                    duration: 1000,
+                    step: function(now, fx) {
 
-    	$('.icon').on('click', function(e) {
-    		e.preventDefault();
-    		var href = $(this).attr('href');
-    		var memberData = memberDataHash[href];
+                    },
+                    start: function() {
+                        $doc.disablescroll();
+                    },
+                    done: function() {
+                        $doc.disablescroll('undo');
+                    }
+                });
+            });
+        });
 
-    		$title.removeClass(memberNames).addClass(href);
-    		$title.find('img').attr('src', '../img/' + href + '.png')
-    						  .attr('height', memberData.iconHeight);
-    		$title.find('.aread').html('');
+        $('.icon').on('click', function(e) {
+            e.preventDefault();
+            var href = $(this).attr('href');
+            var memberData = memberDataHash[href];
 
-    		$map.hide();
-    		$map.filter('.' + href).show();
+            $title.removeClass(memberNames).addClass(href);
+            $title.find('img').attr('src', '../img/' + href + '.png')
+                .attr('height', memberData.iconHeight);
+            $title.find('.aread').html('');
 
-    		$memberInput.val(href);
-    		$areaInput.val(memberData.areaInput);
-    		$areaInput.autocomplete({
-    			source: areaData[href]
-    		});
+            $map.hide();
+            $map.filter('.' + href).show();
 
-    		$p3.fadeIn(300, function() {
+            $memberInput.val(href);
+            $areaInput.val(memberData.areaInput);
+            $areaInput.autocomplete({
+                source: areaData[href]
+            });
 
-	    		$body.animate({
-	                queue: false,
-	                scrollTop: 619 * 2,
-	            }, {
-	                duration: 1000,
-	                step: function(now, fx) {
+            // 2014.10.05新增: 開始
+            stepsLastLine.remove('active');
+            stepsLastCircle.remove('active');
+            // 2014.10.05新增: 結束
 
-	       
-	                },
-	                start: function() {
-	                    $doc.disablescroll();
-	                },
-	                done: function() {
-	                    $doc.disablescroll('undo');
-	                }
-	            });
-    		});
-    	});
+            $p3.fadeIn(300, function() {
 
-    	$('#back').on('click', function(e) {
-    		e.preventDefault();
-			$body.animate({
+                $body.animate({
+                    queue: false,
+                    scrollTop: 619 * 2,
+                }, {
+                    duration: 1000,
+                    step: function(now, fx) {
+
+
+                    },
+                    start: function() {
+                        $doc.disablescroll();
+                    },
+                    done: function() {
+                        $doc.disablescroll('undo');
+                    }
+                });
+            });
+        });
+
+        $('#back').on('click', function(e) {
+            e.preventDefault();
+            $body.animate({
                 queue: false,
                 scrollTop: $p2.offset().top,
             }, {
                 duration: 1000,
                 step: function(now, fx) {
 
-       
+
                 },
                 start: function() {
                     $doc.disablescroll();
@@ -140,29 +163,31 @@
                     $doc.disablescroll('undo');
                 }
             });
-    	});
-		$area.on('mouseover', function(e) {
-    		e.preventDefault();
+        });
+        $area.on('mouseover', function(e) {
+            e.preventDefault();
 
-    		var alt = $(this).attr('alt');
-    		$title.find('.aread').html(alt);
-    	});
+            var alt = $(this).attr('alt');
+            $title.find('.aread').html(alt);
+        });
 
-    	$area.on('click', function(e) {
-    		e.preventDefault();
- 
-    		var alt = $(this).attr('alt');
-    		$areaInput.val(alt);
-    		$formDiv.fadeIn();
-    	});
+        $area.on('click', function(e) {
+            e.preventDefault();
+
+            var alt = $(this).attr('alt');
+            $areaInput.val(alt);
+            $formDiv.fadeIn();
+        });
 
 
-    	$lightboxMsg.find('.header a').on('click', function(e) {
-    		e.preventDefault();
-    		$lightboxMsg.fadeOut();
-    	});
+        $lightboxMsg.find('.header a').on('click', function(e) {
+            e.preventDefault();
+            $lightboxMsg.fadeOut(function() {
+                $lightboxMsgBtnShare.hide();
+            });
+        });
 
-    	// 驗證錯誤訊息
+        // 驗證錯誤訊息
         $.validator.messages = {
             required: '尚未填寫',
             email: '電子郵件信箱格式錯誤',
@@ -181,8 +206,8 @@
         });
 
         $('#area').rules('add', {
-        	required: true,
-        	maxlength: 15
+            required: true,
+            maxlength: 15
         });
 
         $('#name').rules('add', {
@@ -204,40 +229,104 @@
             maxlength: 30
         });
 
+
+        // 2014.10.05新增: 開始
+        var $lightboxMsgContent = $lightboxMsg.find('.content');
+        var $lightboxMsgHeader = $lightboxMsg.find('.header');
+        var $lightboxMsgBtnShare = $lightboxMsg.find('.btn');
+        // 2014.10.05新增: 結束
+
+
         $('#btnSubmit').on('click', function(e) {
-        	e.preventDefault();
+            e.preventDefault();
 
-        	if (!$form.valid()) {
-        		return false;
-        	}
+            if (!$form.valid()) {
+                return false;
+            }
 
-        	var params = $form.serialize();
+            var params = $form.serialize();
 
-        	$lightboxMsg.fadeIn();
-        	$lightboxMsg.find('.content').html('儲存中，請稍後...');
-        	$lightboxMsg.find('.header').css('visibility', 'hidden');
+            $lightboxMsg.fadeIn();
+            $lightboxMsgContent.html('儲存中，請稍後...');
+            $lightboxMsgHeader.css('visibility', 'hidden');
 
-        	$.post('http://xxx.xxx', params, null, 'json').done(function() {
+            // 2014.10.05新增: 開始
+            $lightboxMsgBtnShare.hide();
+            // 2014.10.05新增: 結束
 
-        		var member = $memberInput.val();
-        		$form[0].reset();
-        		$memberInput.val(member);
-        		$areaInput.val(memberDataHash[member].areaInput);
+            $.post('http://xxx.xxx', params, null, 'json').done(function() {
 
-        		$lightboxMsg.find('.content').html('儲存成功，謝謝您的參與！');
-        		$lightboxMsg.find('.header').css('visibility', 'visible');
+                var member = $memberInput.val();
+                $form[0].reset();
+                $memberInput.val(member);
+                $areaInput.val(memberDataHash[member].areaInput);
 
-        	}).fail(function() {
+                // 2014.10.05新增: 開始
+                stepsLastLine.addClass('active');
+                stepsLastCircle.addClass('active');
+                $lightboxMsgBtnShare.show();
+                // 2014.10.05新增: 結束
 
-        		$lightboxMsg.find('.content').html('伺服器沒有回應，請稍後再試');
-        		$lightboxMsg.find('.header').css('visibility', 'visible');
+                $lightboxMsg.find('.content').html('儲存成功，謝謝您的參與！');
+                $lightboxMsg.find('.header').css('visibility', 'visible');
 
-        	}).always(function() {
+            }).fail(function() {
 
-        	});
+                $lightboxMsg.find('.content').html('伺服器沒有回應，請稍後再試');
+                $lightboxMsg.find('.header').css('visibility', 'visible');
+
+            }).always(function() {
+
+            });
         });
 
+
+        // 2014.10.05新增: 開始
+        // preloader, 避免某些atag background-image在hover時才載入導致閃爍
+        (function() {
+            var _urlBase = '../img/';
+            var imgs = [
+                '../img/next.png',
+                '../img/nexth.png',
+                '../img/tsai.png',
+                '../img/tsaih.png',
+                '../img/tsaia.png',
+                '../img/lin.png',
+                '../img/linh.png',
+                '../img/lina.png',
+                '../img/wu.png',
+                '../img/wuh.png',
+                '../img/wua.png',
+                '../img/back.png',
+                '../img/backh.png'
+            ];
+            var i = 0;
+            var image = new Image();
+            image.onerror = image.onload = function() {
+                setTimeout(function() {
+                    return ++i, i >= imgs.length ? (image.onload = null, image.onerror = null, image = null, void(i = null)) : void(image.src = _urlBase + imgs[i]);
+                }, 1);
+            }, image.src = _urlBase + imgs[i];
+        })();
+
+        $('.fbshare').on('click', function() {
+            fbshare();
+        });
+        // 2014.10.05新增: 結束
+
     });
+
+    // 2014.10.05新增: 開始
+    var appId = 1476121305998924;
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=" + appId + "&version=v2.0";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    // 2014.10.05新增: 結束
 
 
 })();
